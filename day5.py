d4cbfacec8112206e396d202bf992ce6eaeb250c
mdatiff592 <mdatiff592@gmail.com>
2026-05-08 20:17:08 +0530

    day5

---
+++ day5.py
@@ -0,0 +1,20 @@
+#loops
+# while loops
+
+count = 1
+while count <= 5:
+    print("hello")
+    count += 1
+
+print(count)
+
+i  = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+a = 5
+while a >=  1:
+    print(a)
+    a -= 1
+
